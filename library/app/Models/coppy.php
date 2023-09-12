d504e93ea7b012d6bddadbab9a32acf6ee98d73d
evingerbono <bono0823@gmail.com>
2023-09-12 13:58:16 +0200

    copy_beállítások

---
+++ library/app/Models/coppy.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class coppy extends Model
+{
+    use HasFactory;
+}
